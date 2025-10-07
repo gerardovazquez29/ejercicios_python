857614c70a2820de7d287a8187a24dbee63be0f0
Gerardo Santiago Vazquez Tovar <neowhatup@gmail.com>
2025-10-06 18:50:02 -0600

    agregado

--- practicas/10_functions_exercises.py
+++ practicas/10_functions_exercises.py
@@ -10,15 +10,40 @@ print(saludo) # Hola desconocido
 # argumentos y retorne el resultado de multiplicarlos.
 def multiply(num1,num2):
     total = num1 * num2
-    return f""
+    return f"El resultado de {num1} x {num2} = {total}"
+print(multiply(5,6)) # El resultado de 5 x 6 = 30
 
-# 3. Crea una función llamada "is_even" que reciba un número entero como argumento y retorne True si es par y False si es impar.
+# 3. Crea una función llamada "is_even" que reciba un número entero 
+# como argumento y retorne True si es par y False si es impar.
+def is_even(num):
+    if num % 2  !=  0:
+        return False
+    return True
+print(is_even(8)) # True
 
-# 4. Escribe una función llamada "convert_to_uppercase" que reciba una cadena de texto y la retorne en mayúsculas.
+# 4. Escribe una función llamada "convert_to_uppercase" que reciba 
+# una cadena de texto y la retorne en mayúsculas.
+def convert_to_uppercase(texto):
+    return texto.upper()
+print(convert_to_uppercase('python es genial'))  #PYTHON ES GENIAL
 
-# 5. Crea una función llamada "arbitrary_sum" que reciba un número arbitrario de números como argumentos y retorne la suma de todos ellos.
+# 5. Crea una función llamada "arbitrary_sum" que reciba un número arbitrario 
+# de números como argumentos y retorne la suma de todos ellos.
+def arbitrary_sum(*args):
+    total = sum(args)
+    return total
+print(arbitrary_sum(2,8,3,9,7,8)) # 37
 
-# 6. Escribe una función llamada "generate_full_greeting" que reciba dos argumentos: nombre y apellido, y retorne el saludo completo "Hola, <nombre> <apellido>". Los argumentos deben ser pasados por clave.
+# 6. Escribe una función llamada "generate_full_greeting" que reciba 
+# dos argumentos: nombre y apellido, y retorne el saludo 
+# completo "Hola, <nombre> <apellido>". 
+# Los argumentos deben ser pasados por clave.
+def generate_full_greeting(**kgars):
+    nombre = kgars.get('nombre' )
+    apellido = kgars.get('apellido')
+    return f"Hola {nombre} {apellido}"
+print(generate_full_greeting(nombre="Gerardo", apellido="Vazquez"))
+# Hola Gerardo Vazquez
 
 # 7. Crea una función llamada "power" que reciba dos números: base y exponente, y retorne el resultado de elevar la base al exponente.
 
